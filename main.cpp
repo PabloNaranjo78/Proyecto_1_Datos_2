7573231e71c7bcc0d08190a70641602941233f79
PabloNaranjo78 <67924944+PabloNaranjo78@users.noreply.github.com>
2021-03-20 20:49:27 -0600

    Se configura gtkmm

--- main.cpp
+++ main.cpp
@@ -1,6 +1,29 @@
-#include <iostream>
+#include <gtkmm.h>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+using namespace Gtk;
+
+int main(int argc, char *argv[])
+{
+    // Inicialización GTK
+    Main entorno(argc, argv);
+
+    // Declaración de objetos
+    Window ventana;
+    Label etiqueta;
+
+    // Características de la ventana
+    ventana.set_title("Hola Mundo!");
+    ventana.set_border_width(5);
+    ventana.set_default_size(400, 200);
+
+    // Etiqueta
+    etiqueta.set_text("Hola Mundo!!");
+    ventana.add(etiqueta);
+
+    // Mostrar
+    ventana.show_all_children();
+
+    entorno.run(ventana);
+
     return 0;
 }
