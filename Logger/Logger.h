fbdfad6f2bef47874a2d040bf7d4c5c7fbd2eca8
Anthony <antoca29@gmail.com>
2021-04-27 00:07:40 -0600

    Documentacion logger

--- Logger/Logger.h
+++ Logger/Logger.h
@@ -9,9 +9,17 @@
 #ifndef PROYECTO_1_DATOS_2_LOGGER_H
 #define PROYECTO_1_DATOS_2_LOGGER_H
 using namespace std;
-
+/**
+ * Clase para realizar logs
+ */
 class Logger {
 public:
+    /**
+     * Genera un mensaje de tipo log, incluyendo su nivel de prioridad
+     * @param lvl nivel de prioridad/gravedad
+     * @param message mensaje
+     * @return log formado a partir del mensaje y su prioridad
+     */
     string logStatement(int lvl, string message);
 };
 
